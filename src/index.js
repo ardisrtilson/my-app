bb55042f40f4425d470bbd00970903ab64f76990
ardisrtilson <email@example.com>
2021-03-08 10:51:45 -0600

    Added canvas draw

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CanvasDraw from "react-canvas-draw";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CanvasDraw />
   </React.StrictMode>,
   document.getElementById('root')
 );
