c0250185c322016cbe271da750f6c60c4cf85241
ardisrtilson <email@example.com>
2021-03-08 11:07:37 -0600

    Added drawshape for testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useContext, useEffect, useState} from 'react';
+import {
+    Line,
+    SteppedLine,
+    PolyLine,
+    Circle,
+    Rectangle
+} from 'draw-shape-reactjs';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div
+                style={{
+                    left: '500px',
+                    height: '100vh',
+                    width: '50vw',
+                    position: 'relative'
+                }}>
+                <PolyLine
+                    position='fixed'
+                    points={[[45, 60], [36, 120], [400, 500], [45, 60]]}
+                    color='#ff8f00'
+                />
+            </div>
+        </div>
+    );
 }
-
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <App />
     <CanvasDraw />
   </React.StrictMode>,
   document.getElementById('root')
